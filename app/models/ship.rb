b6703463db8fa40e878fc187130f3f2e642563c1
Sierra Wallace <sskwallace@gmail.com>
2021-01-28 03:02:28 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty_attributes 
+  @@all = []
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty_attributes = params[:booty_attributes]
+    @@all << self
+  end
+  def self.all 
+    @@all 
+  end
+  def self.clear 
+    @@all.clear 
+  end
 end
